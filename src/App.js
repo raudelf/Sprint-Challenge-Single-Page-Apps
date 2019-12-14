4909017d4d34e8fe0d4b9a714c30c2f3efb07889
Raudel Flores <rawdell@outlook.com>
2019-12-13 20:55:10 -0600

    implemented route within the App.js file and imported the welcome file as well as route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./components/Header.js";
 import CharacterList from './components/CharacterList';
+import Welcome from './components/WelcomePage';
+import { Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList/>
+      <Route exact path='/' component={Welcome}/>
+      <Route path='/characters' component={CharacterList}/>
     </main>
   );
 }
