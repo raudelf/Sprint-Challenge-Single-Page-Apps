d2d1b2f84aef1a62e566d70ecb62fce3026b66b2
Raudel Flores <rawdell@outlook.com>
2019-12-13 21:07:22 -0600

    added links to navigate through the page and styled a bit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import Styled from 'styled-components';
 
 export default function Header() {
+  let LinkContainer = Styled.div`
+  width: 300px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-around;
+  `;
+
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <LinkContainer>
+        <div className="links">
+          <Link to="/">Home</Link>
+        </div>
+        <div className="links">
+          <Link to="/characters">Character List</Link>
+        </div>
+      </LinkContainer>
     </header>
   );
 }
