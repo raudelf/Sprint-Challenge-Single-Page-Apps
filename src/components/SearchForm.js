201d7ad7c5f6f3815bc43f3296a9647f3703d043
Raudel Flores <rawdell@outlook.com>
2019-12-13 21:37:03 -0600

    finished making the search bar

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Card from './CharacterCard';
 
-export default function SearchForm() {
- 
+export default function SearchForm(props) {
+  
+  const [searchName, setSearchName] = useState("");
+
+  const [result, setResult] = useState(props.character);
+
+  useEffect(() => {}, [searchName]);
+
+  const handleChange = event => {
+    setSearchName(event.target.value);
+
+    const results = props.character.filter(characters => {
+      return characters.name.toLowerCase().includes(searchName.toLowerCase());
+    });
+
+    setResult(results);
+  };
+  
   return (
     <section className="search-form">
-     // Add a search form here
+      <form>
+        <input
+          id="name"
+          type="text"
+          name="textfield"
+          placeholder="Search"
+          onChange={handleChange}
+          value={searchName}
+        />
+      </form>
+      <div>
+      {result.map((value, index) => {
+        return (
+          <Card
+            key={index}
+            name={value.name}
+            species={value.species}
+            gender={value.gender}
+            image={value.image}
+          />
+        );
+      })}
+      </div>
     </section>
   );
 }
