27fe119cdf641607dccb24a5877536f2d31c3547
Raudel Flores <rawdell@outlook.com>
2019-12-13 20:43:43 -0600

    finshed laying out the info for the card

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  
+  return <div>
+  <h3>{props.name}</h3>
+  <img src={props.image} alt='Character'/>
+  <p>Species: {props.species}</p>
+  <p>Gender: {props.gender}</p>
+  </div>;
 }
